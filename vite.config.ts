f55dabea261f0d5b58f882933cfb138729d19afc
Paul PZT <150703113+leviathe@users.noreply.github.com>
2025-08-28 20:41:52 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/leviathe.github.io/",
+  base: "/my-website/",
   plugins: [react(), tailwindcss()],
 })
